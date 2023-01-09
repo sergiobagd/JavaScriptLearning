ce41a2a64918c70d2e984afcf44395562c046bd5
Sergey Bagdasaryan <81790360+sergiobagd@users.noreply.github.com>
2023-01-09 11:33:36 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
-// Number
-const num = 42 // integer
-const float = 42.42 // float\
-const pow = 10e3
- 
-console.log('MAX_SAFE_INTEGER ', Number.MAX_SAFE_INTEGER)
-console.log('Math.pow 53', Math.pow(2, 53,) - 1)
-console.log('MIN_SAFE_INTEGER ', Number.MIN_SAFE_INTEGER)
-console.log('MAX VALUE', Number.MAX_VALUE)
-console.log('MIN VALUE', Number.MIN_VALUE)
-console.log('POSITIVE_INFINITY', Number.POSITIVE_INFINITY)
-console.log('NEGATIVE_INFINITY', Number.NEGATIVE_INFINITY)
-console.log('2 / 0', 2 / 0)
-console.log(Number.NaN) // Not A Number
-console.log(typeof NaN)
-const weird = 2 / undefined
-console.log(2 / undefined)
-console.log(Number.isNaN(weird))
-console.log(Number.isNaN(42))
-console.log('Yeaaaah buddy!')
+const name = 'Серёга'
+const age = 20
+
+// const output = 'Привет, меня зовут ' + name + ' и мне ' + age + ' лет.'
+const output = 
+console.log(output)
